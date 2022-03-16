12d36a137b8247989ef1b6047872349e4c552d78
isaac <isaacgc08@gmail.com>
2022-03-15 22:13:48 -0700

    section 3 spacing adjust

--- components/Section3.tsx
+++ components/Section3.tsx
@@ -14,14 +14,14 @@ export default function Section3() {
         </div>
         <div className="cr-content-mt flex flex-col w-full h-full">
           <ul className="flex flex-col lg:flex-row w-full h-full justiy-between items-center">
-            <li className="relative md:w-2/4 xs:mx-4 lg:h-[20rem] xs:h-[18rem] h-[16rem] lg:w-full xs:w-80 w-full flex flex-col justify-start items-center my-8 bg-[#00000011] rounded-xl text-center 2xl:pt-20 lg:pt-12 pt-16 lg:px-4 px-8">
+            <li className="relative md:w-2/4 xs:mx-4 lg:h-[20rem] xs:h-[18rem] h-[16rem] lg:w-full xs:w-80 w-full flex flex-col justify-start items-center my-8 bg-[#00000011] rounded-xl text-center 2xl:pt-20 pt-16 lg:px-4 px-8">
               <div className="absolute 2xl:w-[6rem] w-[5rem] md:top-[-3rem] top-[-2rem] ">
                 <img
                   alt="automatic coupon"
                   src="/icons/automatic_coupons.png"
                 />
               </div>
-              <h3 className="cr-section-subTitle cr-main-color 2xl:h-[40%] h-[40%]">
+              <h3 className="cr-section-subTitle cr-main-color 2xl:h-[40%] h-[35%]">
                 AUTOMATIC COUPONS
               </h3>
               <p className="cr-text h-2/4">
@@ -31,11 +31,11 @@ export default function Section3() {
               </p>
             </li>
 
-            <li className="relative md:w-2/4 xs:mx-4 lg:h-[20rem] xs:h-[18rem] h-[16rem] lg:w-full xs:w-80 w-full flex flex-col justify-start items-center my-8 bg-[#00000011] rounded-xl text-center 2xl:pt-20 lg:pt-12 pt-16 lg:px-4 px-8">
+            <li className="relative md:w-2/4 xs:mx-4 lg:h-[20rem] xs:h-[18rem] h-[16rem] lg:w-full xs:w-80 w-full flex flex-col justify-start items-center my-8 bg-[#00000011] rounded-xl text-center 2xl:pt-20 pt-16 lg:px-4 px-8">
               <div className="absolute 2xl:w-[6rem] w-[5rem] md:top-[-3rem] top-[-2rem] ">
                 <img alt="automatic coupon" src="/icons/loyalty_points.png" />
               </div>
-              <h3 className="cr-section-subTitle cr-main-color 2xl:h-[40%] h-[40%]">
+              <h3 className="cr-section-subTitle cr-main-color 2xl:h-[40%] h-[35%]">
                 LOYALTY POINTS
               </h3>
               <p className="cr-text h-2/4">
@@ -45,14 +45,14 @@ export default function Section3() {
               </p>
             </li>
 
-            <li className="relative md:w-2/4 xs:mx-4 lg:h-[20rem] xs:h-[18rem] h-[16rem] lg:w-full xs:w-80 w-full flex flex-col justify-start items-center my-8 bg-[#00000011] rounded-xl text-center 2xl:pt-20 lg:pt-12 pt-16 lg:px-4 px-8">
+            <li className="relative md:w-2/4 xs:mx-4 lg:h-[20rem] xs:h-[18rem] h-[16rem] lg:w-full xs:w-80 w-full flex flex-col justify-start items-center my-8 bg-[#00000011] rounded-xl text-center 2xl:pt-20 pt-16 lg:px-4 px-8">
               <div className="absolute 2xl:w-[6rem] w-[5rem] md:top-[-3rem] top-[-2rem] ">
                 <img
                   alt="automatic coupon"
                   src="/icons/product_deal_recommendation.png"
                 />
               </div>
-              <h3 className="cr-section-subTitle cr-main-color 2xl:h-[40%] h-[40%]">
+              <h3 className="cr-section-subTitle cr-main-color 2xl:h-[40%] h-[35%]">
                 PRODUCT / DEAL RECOMMENDATION
               </h3>
               <p className="cr-text h-2/4">
