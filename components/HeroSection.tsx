fd17bc9fa9711f4ccbbc27fcb3637fdb2da35e80
isaac <isaacgc08@gmail.com>
2022-03-16 21:44:19 -0700

    expand hero section width and adjust responsiveness

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HeroSection() {
   return (
     <main id="home" className="cr-hero-bg">
-      <div className=" sm:py-16 py-8 h-[960px] w-full container flex flex-col justify-start items-center mb-32">
+      <div className="sm:px-[10%] px-[5%] sm:py-16 py-8 h-[960px] w-full flex flex-col justify-start items-center">
         <div className="flex flex-col justify-center items-center z-10">
           <div className="flex sm:w-40 xs:w-32 w-24">
             <img src="/images/logo_white.png" alt="logo" />
@@ -11,8 +11,8 @@ export default function HeroSection() {
         </div>
         <div className="flex lg:h-full h-fit lg:mt-0 mt-40 lg:flex-row flex-col lg:justify-between justify-center items-center z-10">
           <div className="flex flex-col lg:w-2/4 lg:mr-6 lg:mb-0 mb-16">
-            <h1 className="cr-section-title w-full md:text-left m-0 mb-4">
-              <div className="elementor-divider invisible md:visible">
+            <h1 className="cr-section-title w-full lg:text-left text-center m-0 mb-4">
+              <div className="elementor-divider invisible lg:visible">
                 <div className="elementor-divider-separator"></div>
               </div>
               One-click checkout,
@@ -26,7 +26,7 @@ export default function HeroSection() {
             </article>
           </div>
 
-          <div className="flex xl:w-3/5 lg:w-2/4 w-4/5 h-fit aspect-video lg:ml-6">
+          <div className="flex xl:w-3/5 lg:w-2/4 sm:w-4/5 w-full h-fit aspect-video lg:ml-6">
             <iframe
               width="100%"
               height="100%"
