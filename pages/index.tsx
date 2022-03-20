389bac5976e93db4525e42b766de08b150125e5e
isaac <isaacgc08@gmail.com>
2022-03-20 06:02:11 -0700

    add firebase & firebase hosting

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,19 @@ import Layout from "../components/Layout";
 import Section1 from "../components/Section1";
 import Section2 from "../components/Section2";
 import Section3 from "../components/Section3";
+import { initFirebase } from "../firebase/clientApp";
 
-const IndexPage = () => (
-  <Layout title="Cherry | One-click checkout, wherever you shop">
-    <HeroSection />
-    <Section1 />
-    <Section2 />
-    <Section3 />
-  </Layout>
-);
+initFirebase();
+
+const IndexPage = () => {
+  return (
+    <Layout title="Cherry | One-click checkout, wherever you shop">
+      <HeroSection />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+    </Layout>
+  );
+};
 
 export default IndexPage;
