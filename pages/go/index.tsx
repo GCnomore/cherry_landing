f36c6ffe78db9be72d1a06ef536c511f75192cb8
isaac <isaacgc08@gmail.com>
2022-11-07 23:41:45 -0800

    go page button full width and more margin from the text

--- pages/go/index.tsx
+++ pages/go/index.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const AlmostThere = () => {
   return (
     <div className="flex flex-col h-screen w-screen justify-center items-center">
-      <h1 className="text-3xl mb-2">Almost there!</h1>
+      <h1 className="text-3xl mb-6">Almost there!</h1>
       <a
         href="https://api.cherrycart.app/unilink/setup_completed"
-        className="text-[#da2e5e]"
+        className="text-[#da2e5e] border-[#da2e5e] border border-2 rounded py-2 w-5/6 text-center"
       >
         Click to Complete Setup
       </a>
