1006a1f5e680af3d627f282f81be0628f695de0a
isaac <isaacgc08@gmail.com>
2022-11-08 21:09:07 -0800

    go page button style update

--- pages/go/index.tsx
+++ pages/go/index.tsx
@@ -6,7 +6,7 @@ const AlmostThere: React.FC = () => {
       <h1 className="text-3xl mb-6">Almost there!</h1>
       <a
         href="https://api.cherrycart.app/unilink/setup_completed"
-        className="text-[#da2e5e] border-[#da2e5e] border border-2 rounded py-2 w-5/6 text-center"
+        className="text-[#ffffff] font-bold bg-[#da2e5e] rounded py-2 w-5/6 text-center"
       >
         Click to Complete Setup
       </a>
