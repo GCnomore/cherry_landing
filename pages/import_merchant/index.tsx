2b49c07ef92dc506a55e3c2d3280d34362fad23c
isaac <isaacgc08@gmail.com>
2022-11-08 21:16:55 -0800

    import merchant page style update

--- pages/import_merchant/index.tsx
+++ pages/import_merchant/index.tsx
@@ -4,12 +4,14 @@ import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 const ImportMerchant: React.FC = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <span className="text-2xl mb-5">Go to a merchant website and</span>
-      <span className="text-2xl mb-5">Click the button below</span>
-      <span>
+    <div className="h-screen flex flex-col items-center ">
+      <div className="flex h-full flex-col items-center justify-center">
+        <span className="text-2xl mb-5">Go to a merchant website and</span>
+        <span className="text-2xl mb-20">click the button below</span>
+      </div>
+      <div className="mt-[-4rem]">
         <FontAwesomeIcon icon={faArrowDown} size={"3x"} />
-      </span>
+      </div>
     </div>
   );
 };
