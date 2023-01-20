0a610d2d2aac598755ea01c25e23039a6c205fc2
isaac <isaacgc08@gmail.com>
2023-01-19 21:35:29 -0800

    wording update on /go page

--- pages/go/components/device_detect_modal/DeviceDetectModal.tsx
+++ pages/go/components/device_detect_modal/DeviceDetectModal.tsx
@@ -14,28 +14,28 @@ const DeviceDetectModal: React.FC<IDeviceDetectModal> = ({show}) => {
          <ModalBody>
             <div className='px-4 py-8'>
                <div className='mb-16'>
-                  <h3 className='text-[24px] lg:text-[32px]'>
-                     Please use Safari browser to access CHERRY.<br/>
+                  <h3 className='text-[1.5rem] lg:text-[1.75rem]'>
+                     Please set Safari as a default browser to use CHERRY.<br/><br/>
                      The application is not compatible with other browsers.
                   </h3>
                </div>
                <div>
-                  <h5>How to change your default web browser</h5>
+                  <h5 className='text-[1.25rem]'>How to change your default web browser</h5>
                   <ol className='p-0'>
-                     <li className='mt-2 flex'>
+                     <li className='mt-2 flex text-[1rem]'>
                         <p className='mr-2'>1.</p>
                         <p>Go to Settings and scroll down until you find the browser app or the email app.</p>
                      </li>
-                     <li className='mt-2 flex'>
+                     <li className='mt-2 flex text-[1rem]'>
                         <p className='mr-2'>2.</p>
-                        <p>Tap the app, then tap Default Browser App or Default Mail App.</p>
+                        <p>Tap the app, then tap Default Browser App.</p>
                      </li>
                      <img className='my-4 w-3/4 mx-auto' alt='instruction' src={InstructionImg.src}/>
-                     <li className='flex'>
+                     <li className='flex text-[1rem]'>
                         <p className='mr-2'>3.</p>
                         <p>Select <b>Safari</b> to set it as the default. A checkmark appears to confirm it's the default.</p>
                      </li>
-                     <li className='flex'>
+                     <li className='flex text-[1rem]'>
                         <p className='mr-2'>4.</p>
                         <p>Return to <b>CHERRY</b> and click the link again.</p>
                      </li>
--- pages/go/index.tsx
+++ pages/go/index.tsx
@@ -11,12 +11,12 @@ const AlmostThere: React.FC = () => {
   return (
     <>
       <div className="flex flex-col h-screen w-screen justify-center items-center">
-        <h1 className="text-3xl mb-6">Almost there!</h1>
+        <h1 className="text-3xl mb-6">Setup Completed!</h1>
         <a
           href="https://api.cherrycart.app/unilink/setup_completed"
-          className="text-[#ffffff] font-bold bg-[#da2e5e] rounded py-2 w-5/6 text-center"
+          className="text-[#ffffff] font-bold bg-[#da2e5e] rounded py-2 w-5/6 text-center no-underline"
         >
-          Click to Complete Setup
+          Click to Start CHERRY
         </a>
       </div>
       <DeviceDetectModal show={!isSafari} />
