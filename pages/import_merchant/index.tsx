5157e8e8855cf160f708467ce250e8f3745f1b26
isaac <isaacgc08@gmail.com>
2023-02-16 18:52:38 -0800

    import merchant page update

--- pages/import_merchant/index.tsx
+++ pages/import_merchant/index.tsx
@@ -5,9 +5,11 @@ import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 const ImportMerchant: React.FC = () => {
   return (
     <div className="h-screen flex flex-col items-center ">
-      <div className="flex h-full flex-col items-center justify-center">
-        <span className="text-2xl mb-5">Go to a store website and</span>
-        <span className="text-2xl mb-20">click the button below</span>
+      <div className="flex h-full flex-col justify-center px-8">
+        <h2 className="text-2xl mb-5 text-center">How to import store information</h2>
+        <span className="text-lg mb-5">1. Enter a store's URL or search in this browser</span>
+        <span className="text-lg mb-5">2. Move to the store's website</span>
+        <span className="text-lg mb-20">3. Click the button below to import store's information</span>
       </div>
       <div className="mt-[-4rem]">
         <FontAwesomeIcon icon={faArrowDown} size={"3x"} />
