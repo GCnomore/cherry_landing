86630a1a9dc4f8517da291d5f017e5fce9ad5988
isaac <isaacgc08@gmail.com>
2023-02-16 18:55:38 -0800

    import merchant page update

--- pages/import_merchant/index.tsx
+++ pages/import_merchant/index.tsx
@@ -6,10 +6,10 @@ const ImportMerchant: React.FC = () => {
   return (
     <div className="h-screen flex flex-col items-center ">
       <div className="flex h-full flex-col justify-center px-8">
-        <h2 className="text-2xl mb-5 text-center">How to import store information</h2>
-        <span className="text-lg mb-5">1. Enter a store's URL or search in this browser</span>
-        <span className="text-lg mb-5">2. Move to the store's website</span>
-        <span className="text-lg mb-20">3. Click the button below to import store's information</span>
+        <h2 className="text-xl mb-5 text-center font-bold">How to import store information</h2>
+        <span className="text-base mb-5">1. Enter a store's URL or search in this browser</span>
+        <span className="text-base mb-5">2. Move to the store's website</span>
+        <span className="text-base mb-20">3. Click the button below to import store's information</span>
       </div>
       <div className="mt-[-4rem]">
         <FontAwesomeIcon icon={faArrowDown} size={"3x"} />
